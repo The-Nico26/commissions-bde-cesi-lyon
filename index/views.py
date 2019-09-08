7eeb58b51bc6e43df575f95ca2dca2252a7b08c9
Tanguy <tanguy.blochet@viacesi.fr>
2019-09-08 17:39:09 +0200

    Liste des commissions aléatoires et dernières commissions crées + tags

--- index/views.py
+++ index/views.py
@@ -1,5 +1,25 @@
+import random
+
 from django.shortcuts import render
+
+from commissions.models import Commission
+from commissions.models import Tag
 
 
 def index(request):
-    return render(request, "index.html")
+
+    commissions = Commission.objects.order_by("-creation_date")
+
+    # Get the 5 latest commissions created
+    latest_commissions = commissions[:5]
+    random_commissions = random.sample(list(commissions), 5)
+    tags = Tag.objects.all()
+
+    return render(request, "index.html", {
+        "latest_commissions": latest_commissions,
+        "random_commissions": random_commissions,
+        "tags": tags
+    })
+
+
+
