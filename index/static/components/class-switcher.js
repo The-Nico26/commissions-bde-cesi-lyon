3acdcca73b4d44c992a8234ff5081b30a29fa5cc
EpicKiwi <me@epickiwi.fr>
2019-09-08 14:04:00 +0200

    Redimentionnement du body en mode mobile et implementation d'un menu glissant

---
+++ index/static/components/class-switcher.js
@@ -0,0 +1,64 @@
+// language=HTML
+(() => {
+    /**
+     * @component bde-class-switcher
+     *
+     * Component permettant d'alterner la présence d'une classe sur un élément cible
+     *
+     */
+
+    customElements.define("bde-class-switcher", class extends HTMLElement {
+
+        connectedCallback(){
+            this.addEventListener("click",() => this.switchClass())
+        }
+
+        switchClass(){
+            if(!this.targetClass)
+                return;
+            let targets = this.target ?
+                Array.from(document.querySelectorAll(this.target)) :
+                [this]
+            targets.forEach(el => {
+                if(el.classList.contains(this.targetClass)){
+                    el.classList.remove(this.targetClass)
+                } else {
+                    el.classList.add(this.targetClass)
+                }
+            })
+        }
+
+        get targetClass(){
+            return this.$targetClass
+        }
+
+        set targetClass(val){
+            return this.$targetClass = val
+        }
+
+        get target(){
+            return this.$target
+        }
+
+        set target(val){
+            return this.$target = val
+        }
+
+        static get observedAttributes(){
+            return ['target', 'target-class']
+        }
+
+        attributeChangedCallback(name,lastval,newval){
+            switch(name){
+                case 'target':
+                    this.target = newval
+                    break;
+                case 'target-class':
+                    this.targetClass = newval
+                    break;
+            }
+        }
+
+    })
+
+})()
