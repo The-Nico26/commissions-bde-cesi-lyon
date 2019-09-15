89460a50344739a2c00ed5239b930a5b56bafae6
epickiwi <me@epickiwi.fr>
2019-09-15 16:06:57 +0200

    Ajout d'un champ personnalisé pour la selection des tags

--- commissions/forms.py
+++ commissions/forms.py
@@ -3,11 +3,37 @@ from django import forms
 from commissions.models import Tag
 from commissions.models import User
 
+class TagSelectorWidget(forms.SelectMultiple):
+
+    option_inherits_attrs = True
+    template_name = 'widgets/tag-selector.html'
+    max_selection = None
+
+    def __init__(self, attrs=None, choices=(), max_selection=None):
+        self.max_selection = max_selection
+
+        if self.max_selection is not None:
+            if attrs is not None:
+                attrs = {**attrs, "data-max": max_selection}
+            else:
+                attrs = {"data-max": max_selection}
+
+        super().__init__(attrs, choices)
+
+    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
+
+        if type(self.choices.queryset[index]) is Tag:
+            attributes = {"data-color" : self.choices.queryset[index].color}
+        else:
+            attributes = None
+
+        return super().create_option(name, value, label, selected, index, subindex, attributes)
+
 
 class CreateCommissionForm(forms.Form):
     name = forms.CharField(label='Nom', max_length=100, required=True)
     short_description = forms.CharField(label='Courte description', max_length=200, required=True)
-    tags = forms.ModelMultipleChoiceField(label='Tags', queryset=Tag.objects.all(), to_field_name="name", widget=forms.SelectMultiple)
+    tags = forms.ModelMultipleChoiceField(label='Tags', queryset=Tag.objects.all(), to_field_name="name", widget=TagSelectorWidget(max_selection=3), required=False)
 
     logo = forms.ImageField(required=True, label='Logo')
     banner = forms.ImageField(required=True, label='Bannière')
