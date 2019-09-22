f438333b62c931babe6f1d83c61fb9d8780f8f62
epickiwi <me@epickiwi.fr>
2019-09-22 18:50:33 +0200

    Woopsie

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,4 @@ docker build . -t epickiwi/bdecesi-django
 docker build proxy -t epickiwi/bdecesi-proxy
 
 echo "Starting stack"
-#docker stack deploy --compose-file docker-compose.yml bde-cesi-lyon
-docker-compose up
+docker stack deploy --compose-file docker-compose.yml bde-cesi-lyon
