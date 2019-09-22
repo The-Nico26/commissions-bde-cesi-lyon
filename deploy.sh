bd1cf52aa241b8c074be8e187ae5b793b82cfb2f
epickiwi <me@epickiwi.fr>
2019-09-22 15:13:46 +0200

    correction du deploiement

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,8 @@ ENVIRONMENT=development python src/manage.py collectstatic
 rm -r proxy/static-files
 mv src/static-files proxy/static-files
 
+docker build . -t epickiwi/bdecesi-django
+docker build proxy -t epickiwi/bdecesi-proxy
+
 echo "Starting stack"
 docker stack deploy --compose-file docker-compose.yml bde-cesi-lyon
