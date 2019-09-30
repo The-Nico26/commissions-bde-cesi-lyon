4c670ee2190f2cee5e1e0741a791ac4263201843
epickiwi <me@epickiwi.fr>
2019-09-30 20:29:02 +0200

    Ajout des documents du BDS et instagram

--- src/documents/models.py
+++ src/documents/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Document(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
-    role = models.CharField(max_length=100, choices=[("status", "Status"), ("reglement-interieur", "Règlement interieur")])
-    file = models.FileField(upload_to="documents")
-    current_version = models.BooleanField(default=False)
+    role = models.CharField(max_length=100, choices=[("status", "Status"), ("reglement-interieur", "Règlement interieur"), ("status-bds", "Status du BDS"), ("reglement-interieur-bds", "Règlement interieur du BDS")], help_text="De quel type de document sagit il")
+    file = models.FileField(upload_to="documents", help_text="Le fichier du document pouvant être téléchargé")
+    current_version = models.BooleanField(default=False, help_text="Le document est il la dernière version qui sera affichée sur le site")
--- src/index/context_processor.py
+++ src/index/context_processor.py
@@ -28,9 +28,24 @@ def currentDocuments(request):
     else:
         current_rules = None
 
+    statusBdsQS = Document.objects.order_by("-created_at").filter(role="status-bds", current_version=True)
+    if statusQS.count() > 0:
+        current_status_bds = statusBdsQS[0]
+    else:
+        current_status_bds = None
+
+    rulesBdsQS = Document.objects.order_by("-created_at").filter(role="reglement-interieur-bds", current_version=True)
+    if rulesQS.count() > 0:
+        current_rules_bds = rulesBdsQS[0]
+    else:
+        current_rules_bds = None
+
+
     return {
         'status_doc': current_status,
         'rules_doc': current_rules,
+        'status_bds_doc': current_status_bds,
+        'rules_bds_doc': current_rules_bds,
     }
 
 
