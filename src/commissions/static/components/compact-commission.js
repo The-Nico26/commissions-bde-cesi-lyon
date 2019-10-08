72eb8611344acdba89a3cce45a1022bdb5ec1204
EpicKiwi <me@epickiwi.fr>
2019-10-08 14:22:32 +0200

    Support BDE en sidebar

--- src/commissions/static/components/compact-commission.js
+++ src/commissions/static/components/compact-commission.js
@@ -24,7 +24,8 @@
     .small-commission-logo {
         width: 40px;
         height: 40px;
-        border: solid 5px white;
+        border: solid 2px white;
+        margin: 3px;
         border-radius: 10px;
         background: white;
         overflow: hidden;
@@ -36,9 +37,13 @@
         height: 100%;
         object-fit: cover;
     }
+
+    .bds .small-commission-logo {
+        border-color: var(--bds-color);
+    }
     
     </style>
-    <div class="commission">
+    <div class="commission" id="root">
         <div class="small-commission-logo"><img id="logo" /></div>
         <div class="small-commission-info"><slot></slot></div>
     </div>
@@ -52,6 +57,27 @@
             let template = document.createElement("template")
             template.innerHTML = TEMPLATE
             this.root.appendChild(template.content.cloneNode(true))
+            this.rootEl = this.root.getElementById("root")
+        }
+
+        get organization() {
+            return this.$organization
+        }
+
+        set organization(val) {
+            this.$organization = val
+
+            this.rootEl.classList.remove("bds")
+            this.rootEl.classList.remove("bde")
+
+            switch(this.$organization){
+                case "bde":
+                    this.rootEl.classList.add("bde");
+                    break;
+                case "bds":
+                    this.rootEl.classList.add("bds");
+                    break;
+            }
         }
 
         get logoSrc() {
@@ -64,13 +90,16 @@
         }
 
         static get observedAttributes(){
-            return ['logo-src']
+            return ['logo-src', 'organization']
         }
 
         attributeChangedCallback(name,lastval,newval){
             switch(name){
                 case 'logo-src':
                     this.logoSrc = newval
+                    break;
+                case 'organization':
+                    this.organization = newval
                     break;
             }
         }
