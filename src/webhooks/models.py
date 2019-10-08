47c9e4ffbf874585f28d56bd43ac7c4b81e7a96b
EpicKiwi <me@epickiwi.fr>
2019-10-08 10:38:26 +0200

    Ajout du modèle de données Webhook

--- src/webhooks/models.py
+++ src/webhooks/models.py
@@ -1,3 +1,104 @@
 from django.db import models
+import logging
 
 # Create your models here.
+from raven.transport import requests
+
+logger = logging.getLogger(__name__)
+
+class Webhook(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    name = models.CharField(max_length=255, help_text="Un nom décrivant l'utilisation du WebHook")
+
+    is_active = models.BooleanField(default=True, help_text="Si le WebHook est actif et peut être utilisé")
+
+    event = models.CharField(max_length=100, choices=[
+        ("commission-create", "Creation de commission")
+    ], help_text="L'événement déclanchant l'appel au WebHook")
+
+    type = models.CharField(max_length=100, choices=[
+        ("discord", "Discord")
+    ], help_text="Le format qui doit être utilisé pour éfféctuer la requète")
+
+    url = models.CharField(max_length=512, help_text="L'URL à appeler pour éxécuter le WebHook")
+
+    def trigger(self, force=False, fail_silently=True, **kwargs):
+        if not self.is_active and not force:
+            self.raise_error("Webhook '{}' disabled".format(self.name))
+            return
+        if self.type == "discord":
+            self.trigger_discord(fail_silently=fail_silently, **kwargs)
+        else:
+            self.raise_error("Unimplemented webhook type {}".format(self.type), fail_silently)
+
+    def trigger_discord(self, fail_silently=True, commission=None, commission_url="", commission_logo_url=""):
+        message = ""
+        embeds = []
+
+        if self.trigger == "commission-create":
+
+            message = "Une nouvelle commission débarque : {} !".format(commission.name)
+            embed = {
+                'title': commission.name,
+                'description': commission.short_description,
+                'url': commission_url,  # Si vous voulez faire un lien
+                'thumbnail': {
+                    'url': commission_logo_url
+                },
+                'fields': [
+                ]
+            }
+
+            if len(commission.tags) > 0:
+
+                tags = []
+
+                for tag in commission.tags:
+                    tags += tag.name
+
+                embed["fields"] += {
+                    "name": "Tags",
+                    "value": ", ".join(tags)
+                }
+
+            embed["fields"] += {
+                "name": "Président",
+                "value": commission.president.get_full_name()
+            }
+
+            if commission.treasurer is not None:
+                embed["fields"] += {
+                    "name": "Suppléant",
+                    "value": commission.deputy.get_full_name()
+                }
+
+            if commission.treasurer is not None and commission.treasurer_id != commission.president_id:
+                embed["fields"] += {
+                    "name": "Trésorier",
+                    "value": commission.treasurer.get_full_name()
+                }
+
+            embeds += embed
+
+        else:
+            self.raise_error("Unimplemented webhook discord trigger {}".format(self.type), fail_silently)
+            return
+
+        payload = {
+            "content": message,
+            "embeds": embeds
+        }
+
+        webhookr = requests.post(self.url, json=payload)
+
+        if webhookr.status_code != 200:
+            self.raise_error("Error during webhook request (code {}) : {}".format(webhookr.status_code, webhookr.text))
+
+    def raise_error(self, message, fail_silently=True):
+        if not fail_silently:
+            raise ValueError(message)
+        else:
+            logger.error(message)
