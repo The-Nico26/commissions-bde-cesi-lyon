7875315f2e7fc069c39f0e62846b03decc14d030
EpicKiwi <me@epickiwi.fr>
2019-11-03 17:18:10 +0100

    Ajout user automplete

--- stories/forms.stories.js
+++ stories/forms.stories.js
@@ -77,3 +77,27 @@ stories.add("Selecteur de Tag", () => {
 </bde-tag-selector>
     `
 },{notes:{markdown:TagSelectorDoc}})
+
+import "../src/commissions/static/components/user-autocomplete"
+import "../src/users/static/components/user"
+import UserAutocomplete from "../doc/components/user-autocomplete.md"
+
+stories.add("Autompletion utilisateur", () => {
+    return `
+<style>
+bde-user-autocomplete {
+    max-width: 300px;
+}
+</style>
+<bde-user-autocomplete>
+    <select>
+      <option value="" selected="">---------</option>
+      <option value="-1" data-name="Dupond" data-email="dupond@example.com" data-profile-picture="http://gallery.marvelscollectionscustoms.com/albums/Statues-Collectibles/Weta/Others/Dupont-dupond_04.jpg">Dupond</option>
+      <option value="0" data-name="Dupont" data-email="dupont@example.com" data-profile-picture="http://gallery.marvelscollectionscustoms.com/albums/Statues-Collectibles/Weta/Others/Dupont-dupond_04.jpg">Dupont</option>
+      <option value="1" data-name="Tintin" data-email="tintin@example.com" data-profile-picture="https://pbs.twimg.com/profile_images/479882800737583104/l4l9QYs7.jpeg">Tintin</option>
+      <option value="2" data-name="Tournesol" data-email="tournesol@example.com" data-profile-picture="https://laboussolite.files.wordpress.com/2016/11/image1322.png?w=414">Tournesol</option>
+      <option value="3" data-name="Capitaine Haddock" data-email="capitaine@example.com" data-profile-picture="https://i.pinimg.com/originals/1f/11/6d/1f116d56d3da7b743f30e03a5e5629bb.jpg">Capitaine-haddock</option>
+    </select>
+</bde-user-autocomplete>
+    `
+},{notes:{markdown:UserAutocomplete}})
