686920f744a388b9bfaf1ecdd8704ba3c1019b79
EpicKiwi <me@epickiwi.fr>
2019-11-06 09:30:58 +0100

    Fix export CSV

--- src/adminutils/views.py
+++ src/adminutils/views.py
@@ -1,6 +1,10 @@
+import io
+
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
 from django.shortcuts import render
+import csv
 
 from commissions.models import Commission
 
@@ -11,6 +15,19 @@ def export_commissions(request):
 
 	commissions = Commission.objects.filter(is_active=True, is_organization=False).order_by("name")
 
-	return render(request, "export_commissions.csv", {
-		'commissions': commissions
-	}, content_type="text/csv")
+	final_csv = io.StringIO()
+
+	csv_writer = csv.writer(final_csv, delimiter=';')
+	csv_writer.writerow(["Commission", "Organisation", "Président", "Trésorier", "Suppléant"])
+
+	for com in commissions:
+		data = [
+			com.name,
+			com.organization_dependant,
+			com.president.email,
+			com.treasurer.email if com.treasurer_id != com.president_id else "",
+			com.deputy.email if com.deputy else ""
+		]
+		csv_writer.writerow(data)
+
+	return HttpResponse(final_csv.getvalue(), content_type="text/csv")
