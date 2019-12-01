76b6280751bd2abd325ad9208581e725016309d5
EpicKiwi <me@epickiwi.fr>
2019-12-01 11:30:47 +0100

    Ajout du modèle quicklinks

--- src/index/admin.py
+++ src/index/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from index.models import QuickLink
+
+
+@admin.register(QuickLink)
+class QuickLinkAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('text', 'url')}),
+        ("Organisation", {'fields': ('weight', 'page')}),
+        ("Apparence", {'fields': ('style', 'icon')})
+    )
+    list_display = ('text', 'url', 'page', 'weight')
+    search_fields = ('text', 'url')
+    ordering = ('page', '-weight', 'text')
+    list_filter = ("page", )
--- src/index/models.py
+++ src/index/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class QuickLink(models.Model):
+
+	text = models.CharField(max_length=255, help_text="Court texte de description du lien")
+	url = models.CharField(max_length=255)
+	weight = models.IntegerField(help_text="Importance du lien (les plus importants seront les premiers)", default=0)
+
+	style = models.CharField(max_length=50, choices=[
+		("standard", "Standard"),
+		("special", "Special")
+	], default="standard", help_text="Style du lien")
+	icon = models.CharField(max_length=255, default=None, blank=True, null=True, help_text="Icon du lien (format: mdi-[icon])")
+
+	page = models.CharField(max_length=50, choices=[
+		("index", "Page d'accueil")
+	], default="index", help_text="Page d'affichage du lien")
